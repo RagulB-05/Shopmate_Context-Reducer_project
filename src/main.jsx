5386e842141880a0597ff53801626842cf94d541
RagulB-05 <ragulbala80@gmail.com>
2024-10-11 15:40:36 +0530

    Shopemate_Context-Reducer_Project.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./Context/CardContext.jsx";
+import { CartProvider } from "./Context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
